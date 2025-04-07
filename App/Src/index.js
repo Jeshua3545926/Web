eabfe27f1200aaad2b9819e002cec5bb3dd0c356
Jeshua <garzaromerojeshuaabiram2019ktv@gmail.com>
2025-04-07 03:26:42 -0600

    .

--- App/Src/index.js
+++ App/Src/index.js
@@ -1,12 +1,12 @@
-const emmaButton = document.querySelector('.emma-button');
-const dialog = document.getElementById('emma-dialog');
-const closeDialogButton = document.getElementById('close-dialog');
+// const emmaButton = document.querySelector('.emma-button');
+// const dialog = document.getElementById('emma-dialog');
+// const closeDialogButton = document.getElementById('close-dialog');
 
-emmaButton.addEventListener('click', () => {
-dialog.showModal();
-});
+// emmaButton.addEventListener('click', () => {
+// dialog.showModal();
+// });
 
-closeDialogButton.addEventListener('click', () => {
-dialog.close();
-});
+// closeDialogButton.addEventListener('click', () => {
+// dialog.close();
+// });
 
